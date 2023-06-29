ac8a6e3cf098d7ee8401155a25a1e6b5c3e6528d
Aditya Khandelwal <131429638+AdityaKhandelwal029@users.noreply.github.com>
2023-06-29 15:30:59 +0530

    Add files via upload

---
+++ ATM Plugin Project/src/atm_plugin_project_labelprovider/Addresslabel.java
@@ -0,0 +1,16 @@
+package atm_plugin_project_labelprovider;
+
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+
+import atm_plugin_project_employeedata.*;
+
+public class Addresslabel extends ColumnLabelProvider {
+
+	@Override
+	public String getText(Object element) {
+		// TODO Auto-generated method stub
+		Employeedata account =(Employeedata)element;
+		
+		return String.valueOf(account.getAddress());
+	}
+}
